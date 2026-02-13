a15255c3d4db81fc31ee1ecddff2a001d552137a
sahilDarge <sahil.darge@synradar.com>
2026-02-13 14:28:31 +0530

    Update test webhook

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def calculate():
     expression = request.args.get("expr")
     return str(eval(expression))
 
-print("poooooooo")
+print("test webhook")
 
 if __name__ == "__main__":
     app.run(debug=True)

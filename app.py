809d370270c3a8bd0e87750ee383bbb9b87a7740
sahilDarge <sahil.darge@synradar.com>
2026-02-13 16:58:07 +0530

    7

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def calculate():
     expression = request.args.get("expr")
     return str(eval(expression))
 
-print("prod test 6")
+print("prod testbhb 7")
 
 if __name__ == "__main__":
     app.run(debug=True)

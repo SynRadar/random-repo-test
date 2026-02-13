c0379aff6bf625250adc2d08982613a60f0e936f
sahilDarge <sahil.darge@synradar.com>
2026-02-13 18:20:37 +0530

    prod162

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def calculate():
     expression = request.args.get("expr")
     return str(eval(expression))
 
-print("prod & local 16")
+print("prod 162")
 
 if __name__ == "__main__":
     app.run(debug=True)
